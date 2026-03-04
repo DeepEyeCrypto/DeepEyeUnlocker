758f198111d90a4b317004d2237893677960b4f6
DeepEyeCrypto <nazzo.np@gmail.com>
2026-03-04 22:04:29 +0530

    fix: resolve gradle repository conflict by removing allprojects block

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,6 @@ buildscript {
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
